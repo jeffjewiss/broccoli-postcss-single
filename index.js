7c6bbd949155429bb3a2be0dbfce6de215968f3c
Jeff Jewiss <jeffjewiss@gmail.com>
2017-05-10 15:30:13 -0400

    Update formatting and use of ES6

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
-var path = require('path')
-var fs = require('fs')
-var mkdirp = require('mkdirp')
-var assign = require('object-assign')
-var includePathSearcher = require('include-path-searcher')
-var CachingWriter = require('broccoli-caching-writer')
-var postcss = require('postcss')
+'use strict'
 
-function PostcssCompiler (inputTrees, inputFile, outputFile, options, deprecatedMap) {
+const path = require('path')
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const assign = require('object-assign')
+const includePathSearcher = require('include-path-searcher')
+const CachingWriter = require('broccoli-caching-writer')
+const postcss = require('postcss')
+
+function PostcssCompiler (inputNodes, inputFile, outputFile, options, deprecatedMap) {
   if (!(this instanceof PostcssCompiler)) {
-    return new PostcssCompiler(inputTrees, inputFile, outputFile, options, deprecatedMap)
+    return new PostcssCompiler(inputNodes, inputFile, outputFile, options, deprecatedMap)
   }
 
-  if (!Array.isArray(inputTrees)) {
+  if (!Array.isArray(inputNodes)) {
     throw new Error('Expected array for first argument - did you mean [tree] instead of tree?')
   }
 
-  CachingWriter.call(this, Array.isArray(inputTrees) ? inputTrees : [inputTrees])
+  CachingWriter.call(this, Array.isArray(inputNodes) ? inputNodes : [inputNodes])
 
   this.inputFile = inputFile
   this.outputFile = outputFile
   this.warningStream = process.stderr
 
   if (Array.isArray(options)) {
-    console.warn('The plugin argument has been deprecated, please set your plugins as a property on the options object.')
     this.plugins = options
+    this.warningStream.write('The plugin argument has been deprecated, please set your plugins as a property on the options object.\n')
   } else {
     this.plugins = options.plugins
   }
 
   if (deprecatedMap) {
-    console.warn('The map argument has been deprecated, please set your map configuration as a property on the options object.')
     this.map = deprecatedMap
+    this.warningStream.write('The map argument has been deprecated, please set your map configuration as a property on the options object.\n')
   } else {
     this.map = options.map
   }
+
   this.plugins = this.plugins || []
   this.map = this.map || {}
 }
@@ -42,33 +45,29 @@ PostcssCompiler.prototype = Object.create(CachingWriter.prototype)
 PostcssCompiler.prototype.constructor = PostcssCompiler
 
 PostcssCompiler.prototype.build = function () {
-  var toFilePath = this.outputPath + '/' + this.outputFile
-  var fromFilePath = includePathSearcher.findFileSync(this.inputFile, this.inputPaths)
+  let toFilePath = `${this.outputPath}/${this.outputFile}`
+  let fromFilePath = includePathSearcher.findFileSync(this.inputFile, this.inputPaths)
 
   if (!this.plugins || this.plugins.length < 1) {
     throw new Error('You must provide at least 1 plugin in the plugin array')
   }
 
-  var processor = postcss()
-  var css = fs.readFileSync(fromFilePath, 'utf8')
-  var options = {
+  let processor = postcss()
+  let css = fs.readFileSync(fromFilePath, 'utf8')
+  let options = {
     from: fromFilePath,
     to: toFilePath,
     map: this.map
   }
 
-  this.plugins.forEach(function (plugin) {
-    var pluginOptions = assign(options, plugin.options || {})
+  this.plugins.forEach((plugin) => {
+    let pluginOptions = assign(options, plugin.options || {})
     processor.use(plugin.module(pluginOptions))
   })
 
-  var warningStream = this.warningStream
-
   return processor.process(css, options)
-  .then(function (result) {
-    result.warnings().forEach(function (warn) {
-      warningStream.write(warn.toString())
-    })
+  .then((result) => {
+    result.warnings().forEach(warn => this.warningStream.write(warn.toString()))
 
     mkdirp.sync(path.dirname(toFilePath))
     fs.writeFileSync(toFilePath, result.css, {
@@ -76,14 +75,14 @@ PostcssCompiler.prototype.build = function () {
     })
 
     if (result.map) {
-      fs.writeFileSync(toFilePath + '.map', result.map, {
+      fs.writeFileSync(`${toFilePath}.map`, result.map, {
         encoding: 'utf8'
       })
     }
   })
-  .catch(function (error) {
+  .catch((error) => {
     if (error.name === 'CssSyntaxError') {
-      error.message += '\n' + error.showSourceCode()
+      error.message += `\n${error.showSourceCode()}`
     }
 
     throw error
--- test.js
+++ test.js
@@ -1,16 +1,16 @@
 /* global it, beforeEach, afterEach */
 
-var assert = require('assert')
-var fs = require('fs')
-var path = require('path')
-var broccoli = require('broccoli')
-var postcssCompiler = require('./')
-var postcss = require('postcss')
-var rimraf = require('rimraf')
-var glob = require('glob')
-var async = require('async')
+const assert = require('assert')
+const fs = require('fs')
+const path = require('path')
+const broccoli = require('broccoli')
+const postcssCompiler = require('./')
+const postcss = require('postcss')
+const rimraf = require('rimraf')
+const glob = require('glob')
+const async = require('async')
 
-var basicOptionSet = {
+const basicOptionSet = {
   plugins: [
     {
       module: require('postcss-color-rebeccapurple')
@@ -18,7 +18,7 @@ var basicOptionSet = {
   ]
 }
 
-var testWarnOptionsSet = {
+const testWarnOptionsSet = {
   plugins: [
     {
       module: postcss.plugin('postcss-test-warn', function (opts) {
@@ -30,7 +30,7 @@ var testWarnOptionsSet = {
   ]
 }
 
-var map = {
+const map = {
   inline: false,
   annotation: false
 }
@@ -38,8 +38,8 @@ var map = {
 basicOptionSet.map = map
 testWarnOptionsSet.map = map
 
-var warnings = []
-var warningStreamStub = {
+let warnings = []
+let warningStreamStub = {
   write: function (warning) {
     warnings.push(warning)
   }
@@ -60,12 +60,12 @@ afterEach(function () {
 })
 
 function processCss (outputTree) {
-  var builder = new broccoli.Builder(outputTree) // eslint-disable-line no-new
+  let builder = new broccoli.Builder(outputTree) // eslint-disable-line no-new
   outputTree.warningStream = warningStreamStub
 
   return builder.build().then(function () {
-    var content = fs.readFileSync(path.join(builder.outputPath, 'output.css'), 'utf8')
-    var sourceMap = JSON.parse(fs.readFileSync(path.join(builder.outputPath, 'output.css.map'), 'utf8'))
+    let content = fs.readFileSync(path.join(builder.outputPath, 'output.css'), 'utf8')
+    let sourceMap = JSON.parse(fs.readFileSync(path.join(builder.outputPath, 'output.css.map'), 'utf8'))
 
     assert.strictEqual(content.trim(), 'body {\n  color: rgb(102, 51, 153)\n}')
     assert.strictEqual(sourceMap.mappings, 'AAAA;EACE,wBAAoB;CACrB')
@@ -74,49 +74,49 @@ function processCss (outputTree) {
 }
 
 it('should process css', function () {
-  var outputTree = postcssCompiler(['fixture/success'], 'fixture.css', 'output.css', basicOptionSet)
+  let outputTree = postcssCompiler(['fixture/success'], 'fixture.css', 'output.css', basicOptionSet)
   return processCss(outputTree)
 })
 
-it('should process css using depricated options', function () {
-  var outputTree = postcssCompiler(['fixture/success'], 'fixture.css', 'output.css', basicOptionSet.plugins, basicOptionSet.map)
+it('should process css using deprecated options', function () {
+  let outputTree = postcssCompiler(['fixture/success'], 'fixture.css', 'output.css', basicOptionSet.plugins, basicOptionSet.map)
   return processCss(outputTree)
 })
 
 it('should expose warnings', function () {
-  var outputTree = postcssCompiler(['fixture/warning'], 'fixture.css', 'output.css', testWarnOptionsSet)
-  var builder = new broccoli.Builder(outputTree) // eslint-disable-line no-new
+  let outputTree = postcssCompiler(['fixture/warning'], 'fixture.css', 'output.css', testWarnOptionsSet)
+  let builder = new broccoli.Builder(outputTree) // eslint-disable-line no-new
   outputTree.warningStream = warningStreamStub
 
-  return builder.build().then(function (dir) {
-    var content = fs.readFileSync(path.join(builder.outputPath, 'output.css'), 'utf8')
+  return builder.build().then((dir) => {
+    let content = fs.readFileSync(path.join(builder.outputPath, 'output.css'), 'utf8')
     assert.strictEqual(content.trim(), 'a {}')
     assert.deepEqual(warnings, [ 'postcss-test-warn: This is a warning.' ])
   })
 })
 
 it('should expose syntax errors', function () {
-  var outputTree = postcssCompiler(['fixture/syntax-error'], 'fixture.css', 'output.css', testWarnOptionsSet)
-  var builder = new broccoli.Builder(outputTree) // eslint-disable-line no-new
-  var count = 0
+  let outputTree = postcssCompiler(['fixture/syntax-error'], 'fixture.css', 'output.css', testWarnOptionsSet)
+  let builder = new broccoli.Builder(outputTree) // eslint-disable-line no-new
+  let count = 0
 
   outputTree.warningStream = warningStreamStub
 
   return builder.build()
-  .catch(function (error) {
+  .catch((error) => {
     count++
     assert.strictEqual(error.broccoliPayload.originalError.name, 'CssSyntaxError')
     assert.strictEqual(error.broccoliPayload.originalError.message, `${error.broccoliPayload.originalError.input.file}:1:1: Unknown word\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 1 | \u001b[39ma \u001b[33m}\u001b[39m\n \u001b[90m   | \u001b[39m\u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 2 | \u001b[39m`)
   })
-  .then(function () {
+  .then(() => {
     assert.strictEqual(count, 1)
     assert.deepEqual(warnings, [])
   })
 })
 
 it('should expose non-syntax errors', function () {
-  var outputTree = postcssCompiler(['fixture/missing-file'], 'fixture.css', 'output.css', testWarnOptionsSet)
-  var count = 0
+  let outputTree = postcssCompiler(['fixture/missing-file'], 'fixture.css', 'output.css', testWarnOptionsSet)
+  let count = 0
 
   outputTree.warningStream = warningStreamStub
 
